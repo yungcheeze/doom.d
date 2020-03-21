5f16cc979adadb786d161b33d75e491e2f3c58b8
Ucizi Mafeni <ucizim@gmail.com>
2020-03-21 04:10:51 +0000

    initial doom comfig

---
+++ config.el
@@ -0,0 +1,20 @@
+;;; .doom.d/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here
+(setq display-line-numbers-type nil)
+
+
+(map! "M-+" nil
+      "M-=" nil
+      "M--" nil
+      "C-;" nil)
+
+(map! :leader
+      "t +" #'doom/reset-font-size
+      "t =" #'doom/increase-font-size
+      "t -" #'doom/decrease-font-size)
+
+(map! "C--" #'negative-argument
+      "M--" #'negative-argument)
+
+
